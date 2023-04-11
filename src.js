9811c2f61cdb204dbdcbcbbd6c7b6362759efcb1
meangpu <51825872+meangpu@users.noreply.github.com>
2023-04-11 16:37:25 +0700

    add paragraph break

--- src.js
+++ src.js
@@ -3,6 +3,18 @@ const ConvertedWord = document.getElementById("converted-word");
 
 Textarea.addEventListener("input", (event) => {
   let inputText = event.target.value;
-  let convertedText = inputText.replaceAll("-", "\r\n-");
+  let convertedText = AddNewline(inputText);
+
   ConvertedWord.textContent = convertedText;
 });
+
+function AddNewline(text) {
+  let datNewLine = text.replaceAll("-", "\r\n-");
+  let newParagraph = datNewLine.replaceAll(
+    "..",
+    "\r\n\r\n========================================================"
+  );
+  let finalText = newParagraph.replaceAll("*", "*\r\n");
+
+  return finalText;
+}
